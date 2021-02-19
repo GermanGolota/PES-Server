477b5d43d271091253a7bc4fd45c6e4000688b43
korov <korova2321@gmail.com>
2021-02-19 07:37:48 +0200

    Implemented post message command handler

---
+++ Application/CQRS/Commands/Messages/PostMessage/PostMessageHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts;
+using Application.DTOs;
+using Core.Exceptions;
+using MediatR;
+
+namespace Application.CQRS.Commands
+{
+    public class PostMessageHandler : IRequestHandler<PostMessageCommand, PostMessageResponse>
+    {
+        private readonly IMessageRepo _repo;
+
+        public PostMessageHandler(IMessageRepo repo)
+        {
+            this._repo = repo;
+        }
+        public async Task<PostMessageResponse> Handle(PostMessageCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _repo.AddMessageToChat(request.Message, request.ChatId, request.UserId);
+            }
+            catch(ExpectedException exc)
+            {
+                return new PostMessageResponse
+                {
+                    ResultMessage = exc.Message,
+                    SuccessfullyPosted = false
+                };
+            }
+            catch
+            {
+                return new PostMessageResponse
+                {
+                    ResultMessage = "Something went wrong",
+                    SuccessfullyPosted = false
+                };
+            }
+
+            return new PostMessageResponse
+            {
+                ResultMessage = "Success",
+                SuccessfullyPosted = true
+            };
+        }
+    }
+}
