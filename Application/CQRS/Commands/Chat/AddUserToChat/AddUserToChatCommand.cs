39512c56e3c0661660d5aa2060c3cba984c8330d
korov <korova2321@gmail.com>
2021-02-22 11:30:36 +0200

    Added command to add user to chat

---
+++ Application/CQRS/Commands/Chat/AddUserToChat/AddUserToChatCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.CQRS.Commands
+{
+    public class AddUserToChatCommand : IRequest<CommandResponse>
+    {
+        public Guid ChatId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Application/CQRS/Commands/Chat/AddUserToChat/AddUserToChatHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts;
+using Application.DTOs;
+using Core.Exceptions;
+using MediatR;
+
+namespace Application.CQRS.Commands
+{
+    public class AddUserToChatHandler : IRequestHandler<AddUserToChatCommand, CommandResponse>
+    {
+        private readonly IChatRepo _repo;
+
+        public AddUserToChatHandler(IChatRepo repo)
+        {
+            this._repo = repo;
+        }
+        public async Task<CommandResponse> Handle(AddUserToChatCommand request, CancellationToken cancellationToken)
+        {
+            CommandResponse response;
+            try
+            {
+                await _repo.AddUser(request.ChatId, request.UserId);
+                response = CommandResponse.CreateSuccessfull("Succesfully added user to chat");
+            }
+            catch(ExpectedException exc)
+            {
+                response = CommandResponse.CreateUnsuccessfull(exc.Message);
+            }
+            catch
+            {
+                response = CommandResponse.CreateUnsuccessfull(ExceptionMessages.ServerError);
+            }
+            return response;
+        }
+    }
+}
