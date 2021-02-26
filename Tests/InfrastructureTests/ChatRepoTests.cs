7ef141034e475327ba6d80da15f4b0c7107600da
korov <korova2321@gmail.com>
2021-02-26 12:16:03 +0200

    Renamed PESTests to infrastructure tests

--- Tests/InfrastructureTests/ChatRepoTests.cs
+++ Tests/InfrastructureTests/ChatRepoTests.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace PESTests
+namespace InfrastructureTests
 {
     public class ChatRepoTests : IDisposable
     {
