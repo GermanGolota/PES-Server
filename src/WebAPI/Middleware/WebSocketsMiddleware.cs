a676b58348731d08d51a56e482b6c354bbb5c7df
korov <korova2321@gmail.com>
2021-03-03 00:09:41 +0200

    Added logic for opening and storing websocket connections, as well as deleting them

--- src/WebAPI/Middleware/WebSocketsMiddleware.cs
+++ src/WebAPI/Middleware/WebSocketsMiddleware.cs
@@ -3,26 +3,52 @@ using System;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
+using WebAPI.WebSockets;
 
 namespace WebApi.Middleware
 {
     public class WebSocketsMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IWebSocketsManager _webSocketsManager;
 
-        public WebSocketsMiddleware(RequestDelegate next)
+        public WebSocketsMiddleware(RequestDelegate next, IWebSocketsManager webSocketsManager)
         {
             _next = next;
+            _webSocketsManager = webSocketsManager;
         }
         public async Task InvokeAsync(HttpContext context)
         {
             if (context.WebSockets.IsWebSocketRequest)
             {
                 WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                Guid socketId = _webSocketsManager.AddSocket(webSocket);
+
+                await Receive(webSocket, async (result, buffer) =>
+                {
+                    switch (result.MessageType)
+                    {
+                        case WebSocketMessageType.Close:
+                            await _webSocketsManager.RemoveSocket(socketId);
+                            break;
+                    }
+                });
             }
             else
             {
                 await _next(context);
+            }
+        }
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                                                       cancellationToken: CancellationToken.None);
+                handleMessage(result, buffer);
             }
         }
     }
