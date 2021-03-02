ce684d164d660aa9f5a054e3f30adc618e78e28e
korov <korova2321@gmail.com>
2021-03-03 00:04:03 +0200

    Created basic websocket middleware

---
+++ src/WebAPI/Middleware/WebSocketsMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApi.Middleware
+{
+    public class WebSocketsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public WebSocketsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
