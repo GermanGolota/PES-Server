2ffcc65469be4c79cba70241e2c37b5f711026c6
korov <korova2321@gmail.com>
2021-03-03 00:17:00 +0200

    Added chat websocket model

---
+++ src/WebAPI/WebSockets/ChatWebSocket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace WebAPI.WebSockets
+{
+    public class ChatWebSocket
+    {
+        public Guid ChatId { get; set; }
+        public WebSocket WebSocket{ get; set; }
+    }
+}
