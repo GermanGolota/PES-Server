04b79dad7ec589a126717f306cde8f60d8affff1
korov <korova2321@gmail.com>
2021-03-03 00:04:55 +0200

    Created webSockets manager

---
+++ src/WebAPI/WebSockets/IWebSocketsManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace WebAPI.WebSockets
+{
+    public interface IWebSocketsManager
+    {
+        Guid AddSocket(WebSocket socket);
+        Task RemoveSocket(Guid socketId);
+    }
+}
---
+++ src/WebAPI/WebSockets/WebSocketsManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.WebSockets
+{
+    public class WebSocketsManager : IWebSocketsManager
+    {
+        private Dictionary<Guid, WebSocket> WebSockets { get; set; }
+        public Guid AddSocket(WebSocket socket)
+        {
+            Guid guid = Guid.NewGuid();
+            if (WebSockets.TryAdd(guid, socket))
+            {
+                return guid;
+            }
+            else
+            {
+                throw new Exception("Can't add a socket");
+            }
+        }
+        public async Task RemoveSocket(Guid socketId)
+        {
+
+            if (WebSockets.ContainsKey(socketId))
+            {
+                var webSocket = WebSockets[socketId];
+
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                    "Connection closed as requested", CancellationToken.None);
+
+                WebSockets.Remove(socketId);
+            }
+        }
+    }
+}
