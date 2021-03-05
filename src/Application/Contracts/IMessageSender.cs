509a0b9bd615e11c5f14f2c4c95b9692809a300b
korov <korova2321@gmail.com>
2021-03-05 10:51:48 +0200

    Created service for sending update messages

---
+++ src/Application/Contracts/IMessageSender.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.UpdateMessages;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Application.Contracts
+{
+    public interface IMessageSender
+    {
+        Task SendMessageToSocket(UpdateMessageBase message, Guid chatId);
+    }
+}
---
+++ src/Infrastructure/WebSockets/MessageSender.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts;
+using Application.DTOs.UpdateMessages;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.WebSockets
+{
+    public class MessageSender : IMessageSender
+    {
+        private readonly IWebSocketsManager _webSocketsManager;
+
+        public MessageSender(IWebSocketsManager webSocketsManager)
+        {
+            _webSocketsManager = webSocketsManager;
+        }
+        public async Task SendMessageToSocket(UpdateMessageBase message, Guid chatId)
+        {
+            var separateThread = new Thread(async () => await SendUpdateMessage(chatId, message));
+            separateThread.Priority = ThreadPriority.Normal;
+            separateThread.Start();
+        }
+
+        private async Task SendUpdateMessage(Guid chatId, UpdateMessageBase updateMessage)
+        {
+            await _webSocketsManager.SendJsonToSocketsConnectedToChat(chatId, updateMessage);
+        }
+    }
+}
