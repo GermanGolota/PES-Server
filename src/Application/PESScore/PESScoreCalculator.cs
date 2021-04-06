820da31e1e97c8d40f40cfb9736334fbd3d9f483
korov <korova2321@gmail.com>
2021-04-06 18:41:39 +0300

    Created calculator for pes score

---
+++ src/Application/PESScore/PESScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.PESScore
+{
+    public class PESScoreCalculator
+    {
+        public int CalculateScore(List<string> messages, string pesKey)
+        {
+            List<char> pesChars = pesKey.ToList();
+            long matches = 0;
+            long total = 0;
+            foreach (string message in messages)
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    total++;
+                    char ch = message[i];
+                    if (pesChars.Contains(ch))
+                    {
+                        matches++;
+                    }
+                }
+            }
+            return 100 * (int)(matches / total);
+        }
+    }
+}
