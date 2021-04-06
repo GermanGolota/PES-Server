c0932c315573fd2e8f554d71dba8f3d2486fe768
korov <korova2321@gmail.com>
2021-04-06 19:21:53 +0300

    Added end point for pes score service

---
+++ src/WebAPI/Controllers/ServiceController.cs
@@ -0,0 +1,31 @@
+﻿using Application.CQRS.Queries.Service.GetPesScore;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/service")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ServiceController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("pesScore/{username}")]
+        public async Task<ActionResult<int>> GetPesScore(string username, CancellationToken token)
+        {
+            var query = new GetPesScoreQuery
+            {
+                Username = username
+            };
+            int reponse = await _mediator.Send(query, token);
+            return reponse;
+        }
+    }
+}
