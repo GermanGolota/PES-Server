24e8e9ad08709ae5926a46ee2264ab79b1384f67
korov <korova2321@gmail.com>
2021-04-06 19:15:55 +0300

    Added get pes score query

---
+++ src/Application/CQRS/Queries/Service/GetPesScore/GetPesScoreHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts;
+using MediatR;
+
+namespace Application.CQRS.Queries.Service.GetPesScore
+{
+    public class GetPesScoreHandler : IRequestHandler<GetPesScoreQuery, int>
+    {
+        private readonly IPesScoreService _scoreService;
+        private readonly IPesScoreCalculator _calculator;
+
+        public GetPesScoreHandler(IPesScoreService scoreService, IPesScoreCalculator calculator)
+        {
+            _scoreService = scoreService;
+            _calculator = calculator;
+        }
+        public async Task<int> Handle(GetPesScoreQuery request, CancellationToken cancellationToken)
+        {
+            var scoreModel = await _scoreService.GetPESScoreFor(request.Username);
+            int score = _calculator.CalculateScore(scoreModel);
+            return score;
+        }
+    }
+}
---
+++ src/Application/CQRS/Queries/Service/GetPesScore/GetPesScoreQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Application.CQRS.Queries.Service.GetPesScore
+{
+    public class GetPesScoreQuery : IRequest<int>
+    {
+        public string Username { get; set; }
+    }
+}
