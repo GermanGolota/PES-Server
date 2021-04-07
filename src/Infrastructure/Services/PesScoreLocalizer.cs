105cf7010cb9526151071fb122789df8a72f2632
korov <korova2321@gmail.com>
2021-04-08 00:17:51 +0300

    Added implementation for pes-score localizer

---
+++ src/Infrastructure/Services/PesScoreLocalizer.cs
@@ -0,0 +1,22 @@
+﻿using Application.Contracts.PesScore;
+using Core.Resources;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class PesScoreLocalizer : IPesScoreLocalizer
+    {
+        public string GetLocalizedNameFor(string pesScoreTitle, CultureInfo culture)
+        {
+            string result = null;
+            if(!String.IsNullOrEmpty(pesScoreTitle))
+            {
+                result = PesTitle.ResourceManager.GetString(pesScoreTitle, culture);
+            }
+            return result;
+        }
+    }
+}
