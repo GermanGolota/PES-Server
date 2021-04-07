0b8b48506d8c1c2e5a533f890c6e41a63a415e4a
korov <korova2321@gmail.com>
2021-04-07 23:02:28 +0300

    Created contracts for proper resolution of pes score

---
+++ src/Application/Contracts/PesScore/IPesScoreBadgeLocationResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Contracts.PesScore
+{
+    public interface IPesScoreBadgeLocationResolver
+    {
+        string GetLocationOf(string pesScoreTitle);
+    }
+}
---
+++ src/Application/Contracts/PesScore/IPesScoreLocalizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Application.Contracts.PesScore
+{
+    public interface IPesScoreLocalizer
+    {
+        string GetLocalizedNameFor(string pesScoreTitle, CultureInfo culture);
+    }
+}
