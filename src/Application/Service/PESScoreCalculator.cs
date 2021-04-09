88e0a9b409c3fb3a44a717c1e2e97b1d2f9119cc
korov <korova2321@gmail.com>
2021-04-09 09:43:18 +0300

    Updated pes score calculator to take into account uppercase and lowercase letters

--- src/Application/Service/PESScoreCalculator.cs
+++ src/Application/Service/PESScoreCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Contracts;
 using Application.DTOs.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,7 +10,7 @@ namespace Application.PesScore
     {
         public int CalculateScore(PesScoreModel scoreModel)
         {
-            List<char> pesChars = scoreModel.PesKey.ToList();
+            List<char> pesChars = GetAllLettersWithCounterparts(scoreModel.PesKey);
             long matches = 0;
             long total = 0;
             foreach (string message in scoreModel.Messages)
@@ -25,6 +26,31 @@ namespace Application.PesScore
                 }
             }
             return 100 * (int)(matches / total);
+        }
+
+        private List<char> GetAllLettersWithCounterparts(string str)
+        {
+            var chars = str.ToCharArray();
+            List<char> output = chars.ToList();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char ch = chars[i];
+                if(Char.IsLetter(ch))
+                {
+                    if(Char.IsLower(ch))
+                    {
+                        output.Add(Char.ToUpper(ch));
+                    }
+                    else
+                    {
+                        if(Char.IsUpper(ch))
+                        {
+                            output.Add(Char.ToLower(ch));
+                        }
+                    }
+                }
+            }
+            return output;
         }
     }
 }
