5f3b0bd59da51fdabf57cb1d7ed92e9ab7fac3d0
korov <korova2321@gmail.com>
2021-04-16 12:06:27 +0300

    Added refresh token command

---
+++ src/Application/DTOs/Response/RefreshTokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Response
+{
+    public class RefreshTokenResponse
+    {
+        public bool Successfull { get; set; }
+        public JWTokenModel Token { get; set; }
+    }
+}
