4e42e2abdc16fa4b00d3528c651983e75184cd32
korov <korova2321@gmail.com>
2021-04-19 04:23:31 +0300

    Created tests for token manager

---
+++ Tests/InfrastructureTests/JWTokenManagerTests.cs
@@ -0,0 +1,75 @@
+﻿using Application.Contracts.Repositories;
+using Application.DTOs.Response;
+using Core.Entities;
+using Infrastructure.Authentication;
+using Infrastructure.Config;
+using Infrastructure.Contracts;
+using InfrastructureTests.Mocks;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InfrastructureTests
+{
+    public class JWTokenManagerTests
+    {
+        private readonly JWTokenManager _sut;
+        private readonly IEncrypter _encrypterMock = new EncrypterMock();
+        private readonly Mock<IUserRepo> _repoMock = new Mock<IUserRepo>();
+        public JWTokenManagerTests()
+        {
+            var config = new TokenConfig
+            {
+                EncryptionKey = "This is testing encryption key, please have fun!"
+            };
+
+            _sut = new JWTokenManager(config, _repoMock.Object, _encrypterMock);
+        }
+
+        [Fact]
+        public async Task Authorize_ShouldReturnNull_WhenPasswordDontMatch()
+        {
+            //Arrange
+            string userName = "Oleg";
+            string password = "password123";
+            _repoMock.Setup(x => x.FindUserByUsername(userName)).ReturnsAsync(new User
+            {
+                PasswordHash = "notPassword123"
+            });
+            //Act
+            var actual = await _sut.Authorize(userName, password, CancellationToken.None);
+            //Assert
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public async Task Authorize_ShouldReturnToken_WhenPasswordMatch()
+        {
+            //Arrange
+            string userName = "Oleg";
+            string password = "password123";
+            _repoMock.Setup(x => x.FindUserByUsername(userName)).ReturnsAsync(new User
+            {
+                PasswordHash = password,
+                Username = userName,
+                UserId = Guid.NewGuid()
+            });
+            _repoMock.Setup(x => x.SetRefreshTokenFor(It.IsAny<Guid>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+            //Act
+            JWTokenModel actual = await _sut.Authorize(userName, password, CancellationToken.None);
+            //Assert
+            Assert.NotNull(actual);
+            AssertRefreshTokenBeenSet();
+        }
+
+        private void AssertRefreshTokenBeenSet()
+        {
+            _repoMock.Verify(x => x.SetRefreshTokenFor(It.IsAny<Guid>(), It.IsAny<string>()), Times.Once());
+        }
+    }
+}
---
+++ Tests/InfrastructureTests/Mocks/EncrypterMock.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureTests.Mocks
+{
+    public class EncrypterMock : IEncrypter
+    {
+        public Task<string> Encrypt(string ToBeEncrypt)
+        {
+            return Task.FromResult(ToBeEncrypt);
+        }
+    }
+}
