990cd542215c71633101c4d7aed2d52c77201af4
korov <korova2321@gmail.com>
2021-04-29 07:42:02 +0300

    Added helper method to command response to check for server errors

--- src/Application/DTOs/Response/CommandResponse.cs
+++ src/Application/DTOs/Response/CommandResponse.cs
@@ -1,9 +1,14 @@
-﻿namespace Application.DTOs
+﻿using Core.Exceptions;
+
+namespace Application.DTOs
 {
     public class CommandResponse
     {
         public bool Successfull { get; set; }
         public string ResultMessage { get; set; }
+
+        public bool IsServerError => ResultMessage.Equals(ExceptionMessages.ServerError);
+
         public static CommandResponse CreateSuccessfull(string message)
         {
             return new CommandResponse
