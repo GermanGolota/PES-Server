f454a0fa41b3a0657239555b1c94a1fb1251cfa5
korov <korova2321@gmail.com>
2021-05-10 09:32:08 +0300

    Created query and handler for getting chats of current user

---
+++ src/Application/CQRS/Queries/Chat/GetMyChats/GetMyChatsHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts.Repositories;
+using Application.DTOs;
+using MediatR;
+
+namespace Application.CQRS.Queries
+{
+    public class GetMyChatsHandler : IRequestHandler<GetMyChatsQuery, ChatsModel>
+    {
+        private readonly IChatRepo _repo;
+
+        public GetMyChatsHandler(IChatRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ChatsModel> Handle(GetMyChatsQuery request, CancellationToken cancellationToken)
+        {
+            var chats = await _repo.GetMyChats(request.UserId);
+
+            ChatsModel chatsModel = new ChatsModel
+            {
+                Chats = chats,
+                ChatCount = chats.Count
+            };
+
+            return chatsModel;
+        }
+    }
+}
---
+++ src/Application/CQRS/Queries/Chat/GetMyChats/GetMyChatsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.CQRS.Queries
+{
+    public class GetMyChatsQuery : IRequest<ChatsModel>
+    {
+        public Guid UserId { get; set; }
+    }
+}
