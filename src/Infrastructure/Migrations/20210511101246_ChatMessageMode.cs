d2c82af85fbed5a7667f00d6a9ad06fc94dd9662
korov <korova2321@gmail.com>
2021-05-11 13:18:10 +0300

    Added multi-message field to chat entity

---
+++ src/Infrastructure/Migrations/20210511101246_ChatMessageMode.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class ChatMessageMode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMultiMessage",
+                table: "Chats",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMultiMessage",
+                table: "Chats");
+        }
+    }
+}
