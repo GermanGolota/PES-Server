86ee5282aec0f3bff4737af1070865bf531d7136
korov <korova2321@gmail.com>
2021-05-24 00:05:27 +0300

    Added web socker update model

---
+++ src/Application/DTOs/UpdateMessages/UserKickedUpdateMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.UpdateMessages
+{
+    public class UserKickedUpdateMessage : UpdateMessageBase
+    {
+        public string Username { get; set; }
+    }
+}
