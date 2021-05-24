de7ed5fe3455b5fb5fe53125be769de59a636604
korov <korova2321@gmail.com>
2021-05-24 13:03:39 +0300

    Added command for personal data

---
+++ src/Application/CQRS/Queries/User/PersonalData/PersonalDataCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using Application.DTOs.User;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.CQRS.Commands
+{
+    public class PersonalDataCommand : IRequest<UserProfileModel>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/Application/CQRS/Queries/User/PersonalData/PersonalDataHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts.Repositories;
+using Application.DTOs;
+using Application.DTOs.User;
+using Core.Exceptions;
+using MediatR;
+
+namespace Application.CQRS.Commands
+{
+    public class PersonalDataHandler : IRequestHandler<PersonalDataCommand, UserProfileModel>
+    {
+        private readonly IUserRepo _repo;
+
+        public PersonalDataHandler(IUserRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<UserProfileModel> Handle(PersonalDataCommand request, CancellationToken cancellationToken)
+        {
+            string userName = await _repo.GetUsersUsername(request.UserId);
+            return new UserProfileModel
+            {
+                Username = userName
+            };
+        }
+    }
+}
